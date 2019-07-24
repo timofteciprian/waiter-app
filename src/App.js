926826f809e038d776d8f0871cceb58a787951a6
Unknown <timofteca@gmail.com>
2019-07-24 16:40:10 +0300

    change path

--- src/App.js
+++ src/App.js
@@ -5,27 +5,32 @@ import { Route, Switch } from "react-router-dom";
 import RegistrationForm from "./app/components/auth/Register";
 import LoginForm from "./app/components/auth/Login";
 import Home from "./app/components/Home";
-import Navbar from "./app/components/Navbar";
 import RegistrationRestaurantForm from "./app/components/auth/RegisterRestaurant";
 import Restaurant from "./app/components/restaurant/Restaurant";
 import ReservationTable from "./app/components/restaurant/ReservationTable";
-import Menu from "./app/components/restaurant/Menu";
+import MenuRestaurant from "./app/components/restaurant/MenuRestaurant";
+import RegisterSelect from "./app/components/auth/RegisterSelect";
+import ProfileRestaurant from "./app/components/profile/profileRestaurant";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route path="/registerSelect" component={RegisterSelect} />
         <Route path="/login" component={LoginForm} />
         <Route path="/register" component={RegistrationForm} />
         <Route
           path="/registerRestaurant"
           component={RegistrationRestaurantForm}
         />
-        <Route path="/restaurant" component={Restaurant} />
-        <Route path="/reservationTable" component={ReservationTable} />
-        <Route path="/menu" component={Menu} />
+        <Route exact path="/restaurant" component={Restaurant} />
+        <Route
+          path="/restaurant/reservationTable"
+          component={ReservationTable}
+        />
+        <Route path="/restaurant/menu" component={MenuRestaurant} />
+        <Route path="/profileRestaurant" component={ProfileRestaurant} />
       </Switch>
     </div>
   );
