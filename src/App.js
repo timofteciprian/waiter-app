46045c1031c4437af1cc1d9de6e4c631183f2c8c
Unknown <timofteca@gmail.com>
2019-08-05 21:24:34 +0300

    change route

--- src/App.js
+++ src/App.js
@@ -2,44 +2,46 @@ import React from "react";
 import "./App.css";
 import "antd/dist/antd.css";
 import { Route, Switch } from "react-router-dom";
-import RegistrationForm from "./app/components/auth/Register";
+import { Layout } from "antd";
 import LoginForm from "./app/components/auth/Login";
-import Home from "./app/components/Home";
-import RegistrationRestaurantForm from "./app/components/auth/RegisterRestaurant";
-import Restaurant from "./app/components/restaurant/Restaurant";
-import ReservationTable from "./app/components/restaurant/ReservationTable";
-import MenuRestaurant from "./app/components/restaurant/MenuRestaurant";
-//import MyAccount from "./app/components/myAccount/MyAccount";
-import Navbar from "./app/components/Navbar";
-import Sider from "./app/components/myAccount/Sider";
-import Dashboard from "./app/components/myAccount/Dashboard";
-import AddItem from "./app/components/myAccount/menu/AddItemMenu";
-import ListMenu from "./app/components/myAccount/menu/ListMenu";
+import RegistrationForm from "./app/components/auth/RegisterRestaurant";
+import SiderContent from "./app/components/menuNavigation/Sider";
+import Account from "./app/components/menuNavigation/Account";
+import AddItem from "./app/components/menuNavigation/management/MenuCategories";
+import ItemList from "./app/components/menuNavigation/management/ItemList";
+import HeaderContent from "./app/components/menuNavigation/Header";
+import Home from "./app/components/menuNavigation/Home";
+//import backgroundImg from "./static/Din-out-logo.png";
+
+const { Header, Sider, Content } = Layout;
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/login" component={LoginForm} />
-        <Route path="/register" component={RegistrationForm} />
-        <Route
-          path="/registerRestaurant"
-          component={RegistrationRestaurantForm}
-        />
-        <Route exact path="/restaurant" component={Restaurant} />
-        <Route
-          path="/restaurant/reservationTable"
-          component={ReservationTable}
-        />
-        <Route path="/restaurant/menu" component={MenuRestaurant} />
-        {/* <Route path="/myAccount" component={MyAccount} /> */}
-        <Route exact path="/myAccount" component={Sider} />
-        <Route path="/myAccount/dashboard" component={Dashboard} />
-        <Route path="/myAccount/addItem" component={AddItem} />
-        <Route path="/myAccount/listMenu" component={ListMenu} />
-      </Switch>
+      <Layout>
+        <Header style={{ padding: "0" }}>
+          <HeaderContent />
+        </Header>
+
+        <Layout>
+          <Sider>
+            {" "}
+            <SiderContent />
+          </Sider>
+
+          <Content>
+            <Switch>
+              {/* <Route exact path="/" component={Home} /> */}
+              <Route exact path="/" component={LoginForm} />
+              <Route path="/registerRestaurant" component={RegistrationForm} />
+              <Route exact path="/home" component={Home} />
+              <Route path="/menuManagement/account" component={Account} />
+              <Route path="/menuManagement/addItem" component={AddItem} />
+              <Route path="/menuManagement/listMenu" component={ItemList} />
+            </Switch>
+          </Content>
+        </Layout>
+      </Layout>
     </div>
   );
 }
