886c4dec57196252c3b9289688e5e9a27ec328f9
Unknown <timofteca@gmail.com>
2019-08-06 22:02:51 +0300

    added authorization

--- src/App.js
+++ src/App.js
@@ -1,49 +1,108 @@
 import React from "react";
+
 import "./App.css";
 import "antd/dist/antd.css";
-import { Route, Switch } from "react-router-dom";
+import { Route, Redirect, Switch } from "react-router-dom";
 import { Layout } from "antd";
 import LoginForm from "./app/components/auth/Login";
-import RegistrationForm from "./app/components/auth/RegisterRestaurant";
+import RegistrationForm from "./app/components/auth/Register";
 import SiderContent from "./app/components/menuNavigation/Sider";
 import Account from "./app/components/menuNavigation/Account";
 import AddItem from "./app/components/menuNavigation/management/MenuCategories";
 import ItemList from "./app/components/menuNavigation/management/ItemList";
 import HeaderContent from "./app/components/menuNavigation/Header";
-import Home from "./app/components/menuNavigation/Home";
+import Home from "./app/components/menuNavigation/home/Home";
 //import backgroundImg from "./static/Din-out-logo.png";
 
 const { Header, Sider, Content } = Layout;
 
-function App() {
+const PrivateRoute = ({ component: Component, authed, ...rest }) => {
   return (
-    <div className="App">
-      <Layout>
-        <Header style={{ padding: "0" }}>
-          <HeaderContent />
-        </Header>
-
-        <Layout>
-          <Sider>
-            {" "}
-            <SiderContent />
-          </Sider>
-
-          <Content>
-            <Switch>
-              {/* <Route exact path="/" component={Home} /> */}
-              <Route exact path="/" component={LoginForm} />
-              <Route path="/registerRestaurant" component={RegistrationForm} />
-              <Route exact path="/home" component={Home} />
-              <Route path="/menuManagement/account" component={Account} />
-              <Route path="/menuManagement/addItem" component={AddItem} />
-              <Route path="/menuManagement/listMenu" component={ItemList} />
-            </Switch>
-          </Content>
-        </Layout>
-      </Layout>
-    </div>
+    <Route
+      {...rest}
+      render={props =>
+        authed === true ? (
+          <Layout>
+            <Header style={{ padding: "0" }}>
+              <HeaderContent />
+            </Header>
+            <Layout>
+              <Sider>
+                {" "}
+                <SiderContent />
+              </Sider>
+              <Content>
+                <Component {...props} authed={authed} />
+              </Content>
+            </Layout>
+          </Layout>
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/login",
+              props: props,
+              state: { from: props.location }
+            }}
+          />
+        )
+      }
+    />
   );
+};
+
+function PublicRoute({ component: Component, ...rest }) {
+  return <Route {...rest} render={props => <Component {...props} />} />;
 }
 
-export default App;
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      authed: false
+    };
+  }
+  render() {
+    const token = localStorage.getItem("userToken");
+    const authenticated = !!token;
+
+    return (
+      <div className="App">
+        <Switch>
+          {/* <Route exact path="/" component={Home} /> */}
+          <PublicRoute
+            authed={authenticated}
+            exact
+            path="/login"
+            component={LoginForm}
+          />
+          <PublicRoute
+            authed={authenticated}
+            path="/register"
+            component={RegistrationForm}
+          />
+          <PrivateRoute
+            authed={authenticated}
+            exact
+            path="/home"
+            component={Home}
+          />
+          <PrivateRoute
+            authed={authenticated}
+            path="/menuManagement/account"
+            component={Account}
+          />
+          <PrivateRoute
+            authed={authenticated}
+            path="/menuManagement/addItem"
+            component={AddItem}
+          />
+          <PrivateRoute
+            authed={authenticated}
+            path="/menuManagement/listMenu"
+            component={ItemList}
+          />
+        </Switch>
+      </div>
+    );
+  }
+}
